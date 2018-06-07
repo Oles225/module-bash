ca2b646b30a76b28b4f15684d80005ef2e314787
Oles Didukh <oles.didukh@gmail.com>
2018-06-07 18:17:24 +0300

    ex05.sh

---
+++ ex05/ex05.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sum=0;
+cnt=0;
+
+flag=$1
+
+shift
+
+re='^-?[0-9]+([.][0-9]+)?$'
+for i in $@; do
+if ! [[ $i =~ $re ]] ; then
+   echo "Error..";  exit 1
+fi
+done
+
+case $flag in
+  -s)
+    for i in $@
+    do
+      (( sum += $i ))
+    done
+    echo "$sum";;
+  -e)
+    for i in $@
+    do
+      if !(($i % 2)); then
+      (( sum += $i ))
+      fi
+    done
+    echo "$sum";;
+  -o)
+    for i in $@
+    do
+      if (($i % 2)); then
+      (( sum += $i ))
+      fi
+    done
+    echo "$sum";;
+  -m)
+    for i in $@
+    do
+      (( sum += $i )) && (( cnt++ ))
+    done
+    echo "$(($sum/$cnt))";;
+  *)
+  echo "Error..";;
+esac
